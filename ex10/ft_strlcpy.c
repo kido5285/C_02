bbd2ff4ec1cfe5ab0253573c858e981bbd12b92c
kido5285 <75468540+kido5285@users.noreply.github.com>
2025-03-11 20:35:47 +0800

    latest Update ft_strlcpy.c

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -12,44 +12,44 @@
 
 #include <unistd.h>
 
-unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
+unsigned int    ft_strlcpy(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-	unsigned int	srclen;
+    unsigned int    i;  
+    unsigned int    srclen;
 
-	i = 0;
-	srclen = 0;
-	while (src[i])
-	{
-		srclen++;
-		i++;
-	}
-	if (size <= 0)
-		return (srclen);
-	i = 0;
-	while (i < size - 1)
-	{
-		if (i <= srclen)
-			dest[i] = src[i];
-		else
-			return (srclen)
-		i++;
-	}
-	dest[i] = '\0';
-	return (srclen);
+    i = 0;
+    srclen = 0;
+    while (src[i])
+    {   
+        srclen++;
+        i++;
+    }   
+    if (size <= 0)
+        return (srclen);
+    i = 0;
+    while (i < size - 1)
+    {   
+        if (i <= srclen)
+            dest[i] = src[i];
+        else
+            return (srclen);
+        i++;
+    }
+    dest[i] = '\0';
+    return (srclen);
 }
 
-int	main(void)
+int main(void)
 {
-	char	dest[10] = "lop";
-	char	src[] = "hello";
-	int	j = 0;
+    char    dest[10] = "lop";
+    char    src[] = "hello";
+    int j = 0;
 
-	ft_strlcpy(dest, src, 4);
-	while (dest[j] != '\0')
-	{
-		write(1, &dest[j], 1);
-		j++;
-	}
-	return (1);
+    ft_strlcpy(dest, src, 4); 
+    while (dest[j] != '\0')
+    {   
+        write(1, &dest[j], 1); 
+        j++;
+    }   
+    return (1);
 }
